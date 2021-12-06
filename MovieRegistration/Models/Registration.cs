801429f920fe5b67d6c7a9a62dc8e6ffcfd26bc4
Rick Magdaleno <rickmagdaleno@rocketmortgage.com>
2021-12-06 06:48:30 -0700

    Upload of movie registration MVC lab

---
+++ MovieRegistration/Models/Registration.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieRegistration.Models
+{
+    public class Registration
+    {
+        public string ID { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public int Year { get; set; }
+        public string Actors { get; set; }
+        public string Directors { get; set; }
+    }
+}
